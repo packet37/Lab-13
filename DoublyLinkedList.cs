e662ecef7a5598c33b7e00b2f64a4a3bac0f12bb
packet37 <125910700+packet37@users.noreply.github.com>
2023-05-23 19:45:18 +0300

    Add files via upload

---
+++ DoublyLinkedList.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class DoublyLinkedList : IEnumerable  // двусвязный список
+    {
+        DoublyNode head; // головной/первый элемент
+        DoublyNode tail; // последний/хвостовой элемент
+        int count;  // количество элементов в списке
+
+        // добавление элемента
+        public void Add(int data)
+        {
+            DoublyNode node = new DoublyNode(data);
+
+            if (head == null)
+                head = node;
+            else
+            {
+                tail.Next = node;
+                node.Previous = tail;
+            }
+            tail = node;
+            count++;
+        }
+        public DoublyNode GetHead ()
+        {
+            return head;
+        }
+        public string ToString(DoublyLinkedList a)
+        {
+            return a;
+        }
+        public void AddFirst(int data)
+        {
+            DoublyNode node = new DoublyNode(data);
+            DoublyNode temp = head;
+            node.Next = temp;
+            head = node;
+            if (count == 0)
+                tail = head;
+            else
+                temp.Previous = node;
+            count++;
+        }
+        // удаление
+        public bool Remove(int data)
+        {
+            DoublyNode current = head;
+
+            // поиск удаляемого узла
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    break;
+                }
+                current = current.Next;
+            }
+            if (current != null)
+            {
+                // если узел не последний
+                if (current.Next != null)
+                {
+                    current.Next.Previous = current.Previous;
+                }
+                else
+                {
+                    // если последний, переустанавливаем tail
+                    tail = current.Previous;
+                }
+
+                // если узел не первый
+                if (current.Previous != null)
+                {
+                    current.Previous.Next = current.Next;
+                }
+                else
+                {
+                    // если первый, переустанавливаем head
+                    head = current.Next;
+                }
+                count--;
+                return true;
+            }
+            return false;
+        }
+
+        public int Count { get { return count; } }
+        public bool IsEmpty { get { return count == 0; } }
+
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+        public bool Contains(int data)
+        {
+            DoublyNode current = head;
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                    return true;
+                current = current.Next;
+            }
+            return false;
+        }
+
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            DoublyNode current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+
+        public IEnumerable<int> BackEnumerator()
+        {
+            DoublyNode current = tail;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Previous;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using ConsoleApp1;
+
+DoublyLinkedList linkedList = new DoublyLinkedList();
+
+linkedList.Add(int.Parse(Console.ReadLine()));
+linkedList.Add(int.Parse(Console.ReadLine()));
+string str;
+while ((str=Console.ReadLine()) != "")
+{
+    //if (Math.Abs(str - linkedList.GetHead))
+    linkedList.Add(int.Parse(str));
+}
+
+Console.WriteLine(linkedList.ToString());
+
+//// добавление элементов
+//linkedList.Add(1);
+//linkedList.Add(2);
+//linkedList.Add(3);
+//linkedList.AddFirst(0);
+//foreach (var item in linkedList)
+//{
+//    Console.WriteLine(item);
+//}
+//// удаление
+//linkedList.Remove(2);
+
+//// перебор с последнего элемента
+//foreach (var t in linkedList.BackEnumerator())
+//{
+//    Console.WriteLine(t);
+//}
+//Console.WriteLine(linkedList.Count);    
